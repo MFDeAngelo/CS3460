403f1d381d2a976dc90347426de91a077f35eedb
iamnotpotter <iamnotpotter@gmail.com>
2019-09-23 11:15:39 -0600

    Assignment 3 beginnings

--- Assignment 3 - Google Test/distributions.cpp
+++ Assignment 3 - Google Test/distributions.cpp
@@ -1,39 +1,9 @@
+#include "distributions.hpp"
+
 #include <iomanip>
 #include <iostream>
 #include <random>
 #include <vector>
-
-class DistributionPair
-{
-  public:
-    DistributionPair(std::uint32_t minValue, std::uint32_t maxValue) :
-        minValue(minValue),
-        maxValue(maxValue),
-        count(0)
-    {
-    }
-
-    std::uint32_t minValue;
-    std::uint32_t maxValue;
-    std::uint32_t count;
-};
-
-std::vector<DistributionPair> generateUniformDistribution(std::uint32_t howMany, std::uint32_t min, std::uint32_t max, std::uint8_t numberBins);
-std::vector<DistributionPair> generateNormalDistribution(std::uint32_t howMany, float mean, float stdev, std::uint8_t numberBins);
-std::vector<DistributionPair> generatePoissonDistribution(std::uint32_t howMany, std::uint8_t howOften, std::uint8_t numberBins);
-void plotDistribution(std::string title, const std::vector<DistributionPair>& distribution, const std::uint8_t maxPlotLineSize);
-
-int main()
-{
-    auto uniform = generateUniformDistribution(100000, 0, 79, 40);
-    plotDistribution("--- Uniform ---", uniform, 80);
-    auto normal = generateNormalDistribution(100000, 50, 5, 40);
-    plotDistribution("--- Normal ---", normal, 80);
-    auto poisson = generatePoissonDistribution(100000, 6, 40);
-    plotDistribution("--- Poisson ---", poisson, 80);
-
-    return 0;
-}
 
 std::vector<DistributionPair> generateUniformDistribution(std::uint32_t howMany, std::uint32_t min, std::uint32_t max, std::uint8_t numberBins)
 {
