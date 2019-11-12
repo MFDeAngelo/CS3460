5cb9f6583f776d9af797c461b7c5d4de608f2c43
iamnotpotter <iamnotpotter@gmail.com>
2019-11-12 03:28:16 -0700

    Assignment 7 windows safe.

--- Assignment 7 - Weights/weight.hpp
+++ Assignment 7 - Weights/weight.hpp
@@ -9,75 +9,53 @@ namespace usu
         ct countSize;
 
       public:
-		  weight(ct count) {this->countSize = count;}
-		  weight() {this->countSize = 0;} 
-		  ct count() {return this->countSize;} 
-
-
+        weight(ct count) { this->countSize = count; }
+        weight() { this->countSize = 0; }
+        ct count() { return this->countSize; }
+        using Ratio = ratio;
+        using Ct = ct;
     };
 
-	template <typename ratio, typename ct = uint64_t>
+    template <typename ratio, typename ct = uint64_t>
     weight<ratio, ct> operator+(weight<ratio, ct> w1, weight<ratio, ct> w2)
     {
-        weight<ratio, ct> result();
-        result.countSize = w1.countSize * w2.countSize;
+        weight<ratio, ct> result(w1.count() + w2.count());
         return result;
     }
 
-	template <typename ratio, typename ct = uint64_t>
+    template <typename ratio, typename ct = uint64_t>
     weight<ratio, ct> operator-(weight<ratio, ct> w1, weight<ratio, ct> w2)
     {
-        weight<ratio, ct> result();
-        result.countSize = w1.countSize - w2.countSize;
+        weight<ratio, ct> result(w1.count() - w2.count());
         return result;
     }
 
-	template <typename ratio, typename ct = uint64_t>
-	weight<ratio, ct> operator*(weight<ratio, ct> w, float scalar) {
-        weight<ratio, ct> result();
-        result.countSize = w.countSize * scalar;
-        return result;
-	}
-
-	template <typename ratio, typename ct = uint64_t>
-    weight<ratio, ct> operator*(float scalar, weight<ratio, ct> w)
+    template <typename ratio, typename ct = uint64_t>
+    weight<ratio, ct> operator*(weight<ratio, ct> w, double scalar)
     {
-        weight<ratio, ct> result();
-        result.countSize = w.countSize * scalar;
+        weight<ratio, ct> result(w.count() * static_cast<ct>(scalar));
         return result;
     }
 
+    template <typename ratio, typename ct = uint64_t>
+    weight<ratio, ct> operator*(double scalar, weight<ratio, ct> w)
+    {
+        weight<ratio, ct> result(w.count() * static_cast<ct>(scalar));
+        return result;
+    }
 
-	
-	template <typename weightType>
-    weightType weight_cast( w)
+    template <typename newWeight, typename oldWeight>
+    newWeight weight_cast(oldWeight w)
     {
 
-	}
+        newWeight result(static_cast<typename newWeight::Ct>(w.count() * static_cast<double>(newWeight::Ratio::den * oldWeight::Ratio::num) / static_cast<double>(newWeight::Ratio::num * oldWeight::Ratio::den)));
+        return result;
+    }
 
-
-	using microgram = weight<std::ratio<1,1000000>, uint64_t>;
-    using gram = weight<std::ratio<1, 1>, uint64_t>;
-    using pound = weight<std::ratio<453592, 1000>, uint64_t>;
-    using ounce = weight<std::ratio<2835, 100>, uint64_t>;
-    using ton = weight<std::ratio<1102, 1000000000>, uint64_t>;
-
-
-
+    using microgram = weight<std::ratio<1, 1000000>, uint64_t>;
+    using gram = weight<std::ratio<1, 1>, double>;
+    using pound = weight<std::ratio<453592, 1000>, double>;
+    using ounce = weight<std::ratio<2835, 100>, double>;
+    using ton = weight<std::ratio<1102, 1000000000>, double>;
 
 } // namespace usu
-
-
-/*
-
-Overload the + operator to add two items of the same type.
-Overload the - operator to subtract two items of the same type.
-Overload the * operator to multiply the type by a scalar (integral or floating point).
-These will be defined external to the class; not members of the class.
-You'll need two overloads to do this, one for each position the scalar and weight type can occur;  A scalar times a weight and a weight times a scalar.
-Write a templated weight_cast function (not a member of the weight class) that converts from one weight type to another.  This function is patterned after the std::chrono::duration_cast function.  This function accepts a single template parameter that is the type to convert to, whereas the function parameter is the weight variable to convert from.
-
-
-
-
-*/
